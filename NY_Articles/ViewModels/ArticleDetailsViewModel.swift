575f0cb7900d2e71e2f515b7ea7b92171c2f934f
hager gamal <hager.gamal@linkdev.com>
2021-02-08 00:18:33 +0200

    added missing files

---
+++ NY_Articles/ViewModels/ArticleDetailsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleDetailsViewModel.swift
+//  NY_Articles
+//
+//  Created by hager gamal on 2/7/21.
+//
+
+import Foundation
+
+class ArticleDetailsViewModel {
+    var article : Article?
+    
+    init(article : Article){
+        self.article = article
+    }
+   
+    
+}
