7f3544c26c58041921bef235e55b4a6e5aa38324
hager gamal <hager.gamal@linkdev.com>
2021-02-08 00:19:00 +0200

    remove ignored files

--- Pods/Target Support Files/Pods-NY_Articles-NY_ArticlesUITests/Pods-NY_Articles-NY_ArticlesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NY_Articles_NY_ArticlesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_NY_Articles_NY_ArticlesUITests
-@end
--- Pods/Target Support Files/Pods-NY_Articles/Pods-NY_Articles-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NY_Articles : NSObject
-@end
-@implementation PodsDummy_Pods_NY_Articles
-@end
